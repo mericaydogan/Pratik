93580bf35f262ea4554b4562adbf440aa8e58a42
Meriç <111490745+mericaydogan@users.noreply.github.com>
2023-02-23 18:23:30 +0300

    Add files via upload

---
+++ CA_sayisalLotoAPP/CA_sayisalLotoAPP/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CA_sayisalLotoAPP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] sayiDizisi = new int[6];
+
+            Console.WriteLine("Sayısal Loto");
+            for (int i = 0; i < 6; i++)
+            {
+
+
+                int rastgele = rnd.Next(1, 50);
+                sayiDizisi[i] = rastgele;
+                for (int z = 0; z < i; z++)
+                {
+                    if (sayiDizisi[z] == sayiDizisi[i])
+                    {
+                        i--;
+                        continue;
+                    }
+                }
+
+
+
+            }
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+        }
+
+        }
+    }
+
